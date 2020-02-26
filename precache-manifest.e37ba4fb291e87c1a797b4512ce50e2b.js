ff52d304b2b10545a364377ab3a32b92c40c3a72
MAX <Renad-Ahmad>
2020-02-26 08:45:40 +0300

    Updates

---
+++ precache-manifest.e37ba4fb291e87c1a797b4512ce50e2b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b759f875aa556f127fd6dc8912281ed",
+    "url": "/List-Using-React/index.html"
+  },
+  {
+    "revision": "aa96ba221566aa0eb2c6",
+    "url": "/List-Using-React/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "a3b43c035130f812276e",
+    "url": "/List-Using-React/static/css/main.f0018e53.chunk.css"
+  },
+  {
+    "revision": "aa96ba221566aa0eb2c6",
+    "url": "/List-Using-React/static/js/2.7296564d.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/List-Using-React/static/js/2.7296564d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3b43c035130f812276e",
+    "url": "/List-Using-React/static/js/main.d34377ad.chunk.js"
+  },
+  {
+    "revision": "87c91193271e30f0924c",
+    "url": "/List-Using-React/static/js/runtime-main.7fdc5452.js"
+  }
+]);
