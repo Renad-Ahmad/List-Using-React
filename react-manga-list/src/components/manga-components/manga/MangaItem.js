0cd848d5a090eee3dc1252e3de10856d4f5222fb
MAX <Renad-Ahmad>
2020-02-26 16:00:17 +0300

    add mark As Favorite

--- react-manga-list/src/components/manga-components/manga/MangaItem.js
+++ react-manga-list/src/components/manga-components/manga/MangaItem.js
@@ -2,8 +2,20 @@ import React from 'react';
 import Comments from './comments-cmponents/CommentsMain';
 
 export default class MangaItem extends React.Component{
-  
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      backgroundColor: 'gray'
+    };
+  }
+
+  markAsFavorite = () => {
+    this.setState({ backgroundColor: 'orange'})
+  }
+   
   render(){
+          
     return(
       <div class="row"class="col-6 col-sm-3">
         <img class="rounded" src={this.props.mangaImage} alt="Image" />
@@ -11,12 +23,13 @@ export default class MangaItem extends React.Component{
         <div>
           <h3>{this.props.mangaTitle}</h3>
           <p>Score: {this.props.mangaScore}</p>
-
           <Comments />
         </div>
 
-        <button type= "button" 
-        class="btn btn-outline-light btn btn-secondary" >Favorite</button>
+           {/*    Favorite button   */}
+        <button type= "button" class="btn btn-outline-light btn btn-secondary"  onClick={this.markAsFavorite}
+         style={{backgroundColor:this.state.backgroundColor}}
+        >Favorite</button>
       </div>
         );
       }
